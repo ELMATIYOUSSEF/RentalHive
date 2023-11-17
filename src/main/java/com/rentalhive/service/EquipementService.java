6284bc501c6d74226fb99bf422164d7ad2aeecc5
hajjou walkadi <walkadihajjou153@gmail.com>
2023-11-17 09:48:08 +0100

    adding create method for equipement

--- src/main/java/com/rentalhive/service/EquipementService.java
+++ src/main/java/com/rentalhive/service/EquipementService.java
@@ -1,4 +1,13 @@
 package com.rentalhive.service;
 
+import com.rentalhive.domain.Equipement;
+
+import java.util.List;
+
 public interface EquipementService {
+    List<Equipement> getAllEquipements();
+
+    Equipement getEquipementById(Long id);
+
+    Equipement createEquipement(Equipement equipement);
 }
--- src/main/java/com/rentalhive/service/impl/EquipementServiceImpl.java
+++ src/main/java/com/rentalhive/service/impl/EquipementServiceImpl.java
@@ -1,4 +1,30 @@
 package com.rentalhive.service.impl;
 
-public class EquipementServiceImpl {
+import com.rentalhive.domain.Equipement;
+import com.rentalhive.repository.EquipementRepository;
+import com.rentalhive.service.EquipementService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class EquipementServiceImpl implements EquipementService {
+    
+    private final EquipementRepository equipementRepository;
+    @Autowired
+    public EquipementServiceImpl(EquipementRepository equipementRepository) {
+        this.equipementRepository = equipementRepository;
+    }
+    @Override
+    public List<Equipement> getAllEquipements() {
+        return equipementRepository.findAll();
+    }
+    @Override
+    public Equipement getEquipementById(Long id) {
+        return equipementRepository.findById(id).orElse(null);
+    }
+    @Override
+    public Equipement createEquipement(Equipement equipement) {
+        return equipementRepository.save(equipement);
+    }
+
 }
