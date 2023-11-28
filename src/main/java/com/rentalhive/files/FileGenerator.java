32f685eb1e514b9c4c1486df35934f330cd29529
md_naciri <med.ar.naciri@gmail.com>
2023-11-28 22:08:11 +0100

    Create Devis File

---
+++ src/main/java/com/rentalhive/files/FileGenerator.java
@@ -0,0 +1,51 @@
+package com.rentalhive.files;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class FileGenerator {
+    public static ByteArrayOutputStream fileGenerator(Map<String, Object> rowData, String title) throws DocumentException {
+        Document document = new Document();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PdfWriter.getInstance(document, outputStream);
+        document.open();
+
+        Font titleFont = new Font(Font.FontFamily.HELVETICA, 20, Font.BOLD, BaseColor.BLACK);
+        Paragraph titleParagraph = new Paragraph(title, titleFont);
+        titleParagraph.setAlignment(Element.ALIGN_CENTER);
+        document.add(titleParagraph);
+
+        Font boldFont = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD, BaseColor.BLACK);
+
+        PdfPTable table = new PdfPTable(2);
+        table.setWidthPercentage(100);
+        table.setSpacingBefore(10f);
+
+        for (Map.Entry<String, Object> entry : rowData.entrySet()) {
+            String columnName = entry.getKey();
+            Object value = entry.getValue();
+
+            // Add cell for column name
+            PdfPCell nameCell = new PdfPCell(new Phrase(columnName, boldFont));
+            nameCell.setBorder(Rectangle.NO_BORDER);
+            table.addCell(nameCell);
+
+            // Add cell for column value
+            PdfPCell valueCell = new PdfPCell(new Phrase(value.toString()));
+            valueCell.setBorder(Rectangle.NO_BORDER);
+            table.addCell(valueCell);
+
+            // Add a new line after each pair of name and value
+            table.addCell("");
+            table.addCell("");
+        }
+
+        document.add(table);
+        document.close();
+        return outputStream;
+    }
+}
--- src/main/java/com/rentalhive/web/rest/DevisController.java
+++ src/main/java/com/rentalhive/web/rest/DevisController.java
@@ -1,11 +1,17 @@
 package com.rentalhive.web.rest;
 
+import com.itextpdf.text.DocumentException;
 import com.rentalhive.DTO.DevisDTO;
 import com.rentalhive.DTO.DevisInfoDTO;
 import com.rentalhive.domain.Devis;
 import com.rentalhive.domain.EquipmentItemsReservation;
+import com.rentalhive.files.FileGenerator;
 import com.rentalhive.service.impl.DevisServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,7 +34,8 @@ public class DevisController {
     private DevisServiceImpl devisService;
 
     @PostMapping("/add")
-    public ResponseEntity<Map<String,Object>> addDevis (@Valid @RequestBody DevisDTO devisDTO, BindingResult bindingResult){
+    //public ResponseEntity<Map<String,Object>> addDevis (@Valid @RequestBody DevisDTO devisDTO, BindingResult bindingResult) throws DocumentException, IOException {
+    public ResponseEntity<FileSystemResource> addDevis (@Valid @RequestBody DevisDTO devisDTO, BindingResult bindingResult) throws DocumentException, IOException {
         Map<String, Object> response = new HashMap<>();
         if (bindingResult.hasErrors()){
             response.put("status","Error");
@@ -37,7 +45,8 @@ public class DevisController {
                 errors.put(error.getObjectName(),error.getDefaultMessage());
             }
             response.put("errors",errors);
-            return ResponseEntity.badRequest().body(response);
+            //return ResponseEntity.badRequest().body(response);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
         Devis devis = new Devis();
@@ -56,8 +65,40 @@ public class DevisController {
         Devis devis1 = devisService.addDevis(devis);
         response.put("status","Success");
         response.put("message","DevisAddedSuccessfully");
-        response.put("Devis", devis1.devisToDTO());
-        response.put("Equipments",test);
-        return ResponseEntity.ok(response);
+        response.put("Terms", devis1.devisToDTO().getTerms());
+        response.put("Status", devis1.devisToDTO().getStatus());
+
+        //response.put("Equipment",test);
+        for (int i = 0; i < test.size(); i++) {
+            response.put("Name", test.get(i).getName());
+            response.put("Matricul", test.get(i).getMatricul());
+            response.put("Cout_Location", test.get(i).getCout_Location());
+            response.put("Email", test.get(i).getEmail());
+            response.put("Date Reservation", test.get(i).getDateReservation());
+            response.put("Date retour", test.get(i).getDateRetoure());
+        }
+
+
+        String path = "com/rentalhive/files/";
+        String fname = "devis.pdf";
+        String pfname = path + fname;
+        File directory = new File(path);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        ByteArrayOutputStream devisFile = FileGenerator.fileGenerator(response, "Devis :");
+        try (FileOutputStream fos = new FileOutputStream(pfname)) {
+            fos.write(devisFile.toByteArray());
+        }
+        FileSystemResource fileResource = new FileSystemResource(new File(pfname));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fname);
+        headers.setContentLength(fileResource.contentLength());
+
+        return new ResponseEntity<>(fileResource, headers, HttpStatus.OK);
+
+        //return ResponseEntity.ok(response);
     }
 }
